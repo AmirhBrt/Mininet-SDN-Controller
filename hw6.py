72df23ac94f3fa5e3dbf202631e03c24d01421ab
Amirhbrt <amirhsn.barati@gmail.com>
2023-07-07 16:50:58 +0330

    +added dijkstra to find min delay of two nodes

--- hw6.py
+++ hw6.py
@@ -53,21 +53,68 @@ class MyTopo(Topo):
         self.addLink(host8, switch8, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
 
         #Add switches links
-        self.addLink(switch1, switch3, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch1, switch8, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch2, switch4, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch2, switch5, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch2, switch7, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch3, switch4, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch3, switch6, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch3, switch8, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch4, switch5, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch4, switch7, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch5, switch6, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch5, switch7, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch6, switch8, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
-        self.addLink(switch7, switch8, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch1, switch3, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch1, switch8, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch2, switch4, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch2, switch5, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch2, switch7, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch3, switch4, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch3, switch6, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch3, switch8, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch4, switch5, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch4, switch7, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch5, switch6, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch5, switch7, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch6, switch8, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
+        self.addLink(switch7, switch8, cls=TCLink, bw=BAND_WIDTH, delay=f'{randint(1, DELAY_MAX)}ms')
 
+
+def get_path_to_src(node, parents):
+    if parents[node] == -1:
+        return f'{node} '
+    return get_path_to_src(parents[node], parents) + f'{node} '
+ 
+
+def dijkstra(network, src_node):
+    all_nodes = network.hosts + network.switches
+    # Initialize dist to MAX and parent to None
+    dist = {}
+    parent = {}
+    added = {}
+    for i in all_nodes:
+        dist[i.name] = float('inf')
+        added[i.name] = False
+
+    dist[src_node.name] = 0
+    parent[src_node.name] = -1
+    
+    #main loop
+    for i in range(len(all_nodes)):
+        #Find Node with the shortest path from src
+
+        nearest_node = None
+        shortest_path = float('inf')
+
+        for node in all_nodes:
+            name = node.name
+            if not added[name] and dist[name] < shortest_path:
+                nearest_node = node
+                shortest_path = dist[name]
+
+        #Mark node
+        added[nearest_node.name] = True
+        #Update distance
+        for node in all_nodes:
+            links = network.linksBetween(nearest_node, node)
+            if links:
+                link = links[0]
+                delay = int(link.intf1.params['delay'].replace('ms', ''))
+
+                if shortest_path + delay < dist[node.name]:
+                    dist[node.name] = shortest_path + delay
+                    parent[node.name] = nearest_node.name
+
+    return dist, parent
 
 def emptyNet():
 
@@ -77,9 +124,21 @@ def emptyNet():
     net = Mininet(topo=topo, switch=MySwitch, build=False)
     #Adding Remote Controller
     net.addController(c0)
-    
-    #Build the network and start it
+
+    #Build Network with given topology
     net.build()
+
+    #Find route with least delay
+
+    src = input('Enter source node name: ')
+    dst = input('Enter destination node name: ')
+
+    src_node = net.getNodeByName(src)
+
+    dist_from_src, parents = dijkstra(net, src_node)
+    print(f'Distance {src} from {dst} is \"{dist_from_src[dst]}ms\" using path: {get_path_to_src(dst, parents)}')
+
+    #Build the network and start it
     net.start()
 
     #Start CLI
